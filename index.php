bca60db845cd27e05f7683c57fb5ddca5a56625d
Nnamani Chinaecherem <joshchief169@gmail.com>
2021-08-19 19:43:20 +0100

    final commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+
+    <style>
+        .name{
+           text-transform: uppercase;
+           font-size: 30px;
+           text-align: center;
+           border: 5px solid black;
+           width: 50%;
+           margin: 60px auto;
+        }
+    </style>    
+
+
+    <body>
+        <div class="logo">
+            <header>
+                <img src="/img/zuri-logo.jpg" alt="">
+            </header>
+        </div>
+
+        <div class="name">
+
+            <h4>
+
+                <?php 
+
+
+                    $first_name = 'Joshua';
+                    $last_name = 'Nnamani';
+
+
+                
+
+                    echo "My name is $first_name  $last_name";
+            
+                ?>
+            </h4>
+
+            
+
+        </div>
+    </body>
+</html>
